de16bea38075cf72e4aedf096659de5cc1fcd105
ApexOuroboros <114581783+ApexOuroboros@users.noreply.github.com>
2023-02-20 15:25:37 +0000

    can place

--- Assets/scripts/buildingDown.cs
+++ Assets/scripts/buildingDown.cs
@@ -7,10 +7,46 @@ public class buildingDown : MonoBehaviour
     private bool curPlace;
     private placeBuild curBuildPlace;
 
+    private Vector3 placementPos;
+
     public static buildingDown inst;
 
     void Awake()
     {
         inst = this;
     }
+
+    public void StartPlacing(placeBuild placeBuild)
+    {
+        curPlace = true;
+        curBuildPlace = placeBuild;
+    }
+
+    public void CancelPlacing()
+    {
+        curPlace = false;
+    }
+
+    void PlaceingTime()
+    {
+        GameObject gameObject = Instantiate(curBuildPlace.prefab, placementPos, Quaternion.identity);
+        gManage.inst.OnPlaceObject(curBuildPlace);
+
+        CancelPlacing();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            CancelPlacing();
+        }
+
+        if(curPlace && Input.GetMouseButtonDown(0))
+        {
+            PlaceingTime();
+        }
+
+    }
+
 }
